286a8cea930ea8bc367962f15f80e82159fa5c95
mohammadfaizansari786 <mohammadfaizansari2007@gmail.com>
2025-12-06 17:52:37 +0530

    Create 20250520180813_initial.sql

---
+++ crates/timescale/migrations/20250520180813_initial.sql
@@ -0,0 +1,25 @@
+create table timing_driver (
+    time    timestamptz not null default now(),
+    nr      text        not null,
+    lap     integer,
+    gap             bigint,
+    leader_gap      bigint,
+    laptime         bigint,
+    sector_1        bigint,
+    sector_2        bigint,
+    sector_3        bigint
+);
+create index on timing_driver (nr, time desc);
+select create_hypertable('timing_driver', 'time');
+select add_retention_policy('timing_driver', interval '4 hours');
+
+create table tire_driver (
+    time    timestamptz not null default now(),
+    nr      text        not null,
+    lap     integer,
+    compound   text,
+    laps       integer
+);
+create index on tire_driver (nr, time desc);
+select create_hypertable('tire_driver', 'time');
+select add_retention_policy('tire_driver', interval '4 hours');
