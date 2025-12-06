80a1ff172a5d04e67d26c1e682585f70b1d78ab2
mohammadfaizansari786 <mohammadfaizansari2007@gmail.com>
2025-12-06 18:07:50 +0530

    Update health.rs

--- services/analytics/src/server/health.rs
+++ services/analytics/src/server/health.rs
@@ -1,6 +1,6 @@
 use axum::{http::StatusCode, response::IntoResponse, Json};
 use serde_json::json;
 
-pub async fn healt_check() -> impl IntoResponse {
+pub async fn health_check() -> impl IntoResponse {
     (StatusCode::OK, Json(json!({ "success": true })))
 }
